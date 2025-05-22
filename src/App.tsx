21be0e98bdd7b70aebaca7fc10b10d987d4f5d94
rifat-hasan <mdrifathasanshaun@gmail.com>
2025-05-23 05:26:49 +0600

    fix: theme issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import theme from './styles/theme';
+import theme from './Theme';
 import queryClient from './store/queryClient';
 import store from './store';
-import { publicRoutes } from './routes/routeConfig';
-import { protectedRoutes } from './routes/routeConfig';
+import { publicRoutes, protectedRoutes } from './routes/routeConfig';
 
 function App() {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
-          <CssBaseline />
           <CssBaseline />
           <RouterProvider router={createBrowserRouter([...publicRoutes, ...protectedRoutes])} />
         </ThemeProvider>
